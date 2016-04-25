f34ebcb400771469fb5246c6d5905834032ef2a9
Matthew Caldwell <caldwellysr@gmail.com>
2016-04-25 17:25:08 -0400

    add classes and races

--- scripts/controllers/character.js
+++ scripts/controllers/character.js
@@ -1,13 +1,19 @@
 "use strict";
 
 angular.module('characterCreation')
-.controller('characterCtrl', function($scope, statsService) {
+.controller('characterCtrl', function($scope, dataService) {
     $scope.character = {
         "name": "Character Name",
-        "race": "Human"
+        "race": "Human",
+        "points": 0
     };
-    statsService.getStats(function(response) {
-        console.log(response.data);
+    dataService.getStats(function(response) {
         $scope.character.stats = response.data;
+    });
+    dataService.getRaces(function(response) {
+        $scope.races = response.data;
+    });
+    dataService.getClasses(function(response) {
+        $scope.classes = response.data;
     });
 });
--- scripts/services/stats.js
+++ scripts/services/stats.js
@@ -1,8 +1,14 @@
 "use strict";
 
 angular.module('characterCreation')
-.service('statsService', function($http) {
+.service('dataService', function($http) {
     this.getStats = function(callback) {
         $http.get('mock/stats.json').then(callback);
+    };
+    this.getClasses = function(callback) {
+        $http.get('mock/classes.json').then(callback);
+    };
+    this.getRaces = function(callback) {
+        $http.get('mock/races.json').then(callback);
     };
 });
